ca078a5bbeed82da5b06a8ca29b5cac90b60d1a1
kaustubhchile <kaustubchile@gmail.com>
2023-12-23 12:00:56 +0530

    All files added

---
+++ script.js
@@ -0,0 +1,89 @@
+const dropList = document.querySelectorAll("form select"),
+  fromCurrency = document.querySelector(".from select"),
+  toCurrency = document.querySelector(".to select"),
+  getButton = document.querySelector("form button");
+
+for (let i = 0; i < dropList.length; i++) {
+  for (let currency_code in country_list) {
+    let selected =
+      i == 0
+        ? currency_code == "USD"
+          ? "selected"
+          : ""
+        : currency_code == "NPR"
+        ? "selected"
+        : "";
+    let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
+    dropList[i].insertAdjacentHTML("beforeend", optionTag);
+  }
+  dropList[i].addEventListener("change", (e) => {
+    loadFlag(e.target);
+  });
+}
+
+function loadFlag(element) {
+  for (let code in country_list) {
+    if (code == element.value) {
+      let imgTag = element.parentElement.querySelector("img");
+      imgTag.src = `https://flagcdn.com/48x36/${country_list[
+        code
+      ].toLowerCase()}.png`;
+    }
+  }
+}
+
+window.addEventListener("load", () => {
+  getExchangeRate();
+});
+
+getButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  getExchangeRate();
+});
+
+const exchangeIcon = document.querySelector("form .icon");
+exchangeIcon.addEventListener("click", () => {
+  let tempCode = fromCurrency.value;
+  fromCurrency.value = toCurrency.value;
+  toCurrency.value = tempCode;
+  loadFlag(fromCurrency);
+  loadFlag(toCurrency);
+  getExchangeRate();
+});
+
+function getExchangeRate() {
+  const amount = document.querySelector("form input");
+  const exchangeRateTxt = document.querySelector("form .exchange-rate");
+  let amountVal = amount.value;
+
+  if (amountVal == "" || amountVal == "0") {
+    amount.value = "1";
+    amountVal = 1;
+  }
+
+  exchangeRateTxt.innerText = "Getting exchange rate...";
+
+  let url = `https://v6.exchangerate-api.com/v6/9f6561165307ec88f52dec9a/latest/${fromCurrency.value}`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((result) => {
+      if (result.conversion_rates) {
+        let exchangeRate = result.conversion_rates[toCurrency.value];
+        if (exchangeRate !== undefined) {
+          let totalExRate = (amountVal * exchangeRate).toFixed(2);
+          exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExRate} ${toCurrency.value}`;
+        } else {
+          exchangeRateTxt.innerText = `Exchange rate for ${toCurrency.value} not available`;
+        }
+      } else {
+        exchangeRateTxt.innerText =
+          "Conversion rates not available in the API response";
+      }
+      // console.log(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      exchangeRateTxt.innerText = "Something went wrong";
+    });
+}
